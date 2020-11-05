912c49ee4a9c6f710bd819c87ebfb32d1736606c
rishavkumar123 <vikrantsingh7384141074@gmail.com>
2020-11-05 12:18:05 +0530

    sidebar removed

--- resources/views/admin/users/sidebar.blade.php
+++ resources/views/admin/users/sidebar.blade.php
@@ -46,7 +46,7 @@
                     Template Messages
                 </a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span data-feather="users"></span>
                     Customers
@@ -63,17 +63,17 @@
                     <span data-feather="layers"></span>
                     Integrations
                 </a>
-            </li>
+            </li> --}}
         </ul>
 
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        {{-- <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
             <span>Saved reports</span>
             <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
                 <span data-feather="plus-circle"></span>
             </a>
-        </h6>
+        </h6> --}}
         <ul class="nav flex-column mb-2">
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span data-feather="file-text"></span>
                     Current month
@@ -90,7 +90,7 @@
                     <span data-feather="file-text"></span>
                     Social engagement
                 </a>
-            </li>
+            </li> --}}
             <li class="nav-item">
                 @can('manage-users')
                 <a class="nav-link" href="{{route('admin.users.index')}}">
